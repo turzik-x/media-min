f699d0edaa9243566208672bd06963110f239064
lisonum <ludek.turzik@gmail.com>
2022-06-14 20:53:49 +0200

    add ffmpeg, jpg files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # media-min
 Script for converting photos and media to a smaller size. Script uses FFmpeg and ExifTool utilities.
+
+## use ffmpeg-python (convert media files)
+https://github.com/kkroening/ffmpeg-python
+
+## use exiftool (get metainfo from media files)
+python -m pip install -U pyexiftool
--- media-convert.py
+++ media-convert.py
@@ -1,1 +1,26 @@
-print("hello")
+#!/usr/bin/python
+
+from distutils import extension
+import sys
+import ffmpeg
+import os
+
+
+
+def media_convert(filename):
+    ext = os.path.splitext(filename)[1]
+    print (f"filename:{filename}")
+    import ffmpeg
+    (
+      ffmpeg
+      .input(filename)
+      .output( filename + ".min."+ext  )
+      .run()
+    )
+
+
+def main(argv):
+    media_convert(argv[1])
+    pass
+
+main(sys.argv)
